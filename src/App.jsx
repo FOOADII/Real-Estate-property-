2a52eb94bc24a47dd12c3f594517cf4cfe4f2884
FUAD GETACHEW <fuadgetachew2@gmail.com>
2023-12-09 08:28:45 +0300

    mongo changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import About from './pages/about';
+import SignIn from './pages/signIn';
+import SignOut from './pages/signOut';
+import Profile from './pages/profile';
+import Header from './components/header';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signOut" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
